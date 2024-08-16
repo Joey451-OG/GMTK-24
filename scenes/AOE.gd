7a7ac08b203e40ea26a7423fd49ae1ff82c38f7f
ItIsJoeyG <itsjoeygofficial@outlook.com>
2024-08-16 18:17:57 -0500

    Added box ID system

--- scenes/AOE.gd
+++ scenes/AOE.gd
@@ -1,30 +1,34 @@
 extends Node2D
 
-@onready var box_list = [$Box]
-@onready var point_cloud = [
-$orbital_zone/pivot/Node2D, 
-$orbital_zone/pivot/Node2D2,
-$orbital_zone/pivot/Node2D3, 
-$orbital_zone/pivot/Node2D4, 
-$orbital_zone/pivot/Node2D5, 
-$orbital_zone/pivot/Node2D6, 
-$orbital_zone/pivot/Node2D7, 
-$orbital_zone/pivot/Node2D8
-]
+@export var pivot_num := 3
 
-@onready var pivot = $orbital_zone/pivot
+
+@onready var box_list = {}
+
+@onready var pivot_3 = [$orbital_zone/pivot_3/Node2D, $orbital_zone/pivot_3/Node2D2, $orbital_zone/pivot_3/Node2D3]
+@onready var pivot_4 = [$orbital_zone/pivot_4/Node2D, $orbital_zone/pivot_4/Node2D2, $orbital_zone/pivot_4/Node2D3, $orbital_zone/pivot_4/Node2D4]
+@onready var pivot_5 = [$orbital_zone/pivot_5/Node2D, $orbital_zone/pivot_5/Node2D2, $orbital_zone/pivot_5/Node2D3, $orbital_zone/pivot_5/Node2D4, $orbital_zone/pivot_5/Node2D5]
+@onready var pivot_6 = [$orbital_zone/pivot_6/Node2D, $orbital_zone/pivot_6/Node2D2, $orbital_zone/pivot_6/Node2D3, $orbital_zone/pivot_6/Node2D4, $orbital_zone/pivot_6/Node2D5, $orbital_zone/pivot_6/Node2D6]
+
+@onready var pivot = $orbital_zone/pivot_3
+
+@onready var clouds := [pivot_3, pivot_4, pivot_5, pivot_6]
 
 func _process(delta) -> void:
-	_update_box_list()
 	_rotate_point_cloud(delta, 1.4)
-	for _box in box_list:
-		_calculate_box_logic(_box, point_cloud[0].global_position, delta)
+	var begin_i = 0
+	for box in box_list.keys():
+		_calculate_box_logic(box_list[box], clouds[pivot_num - 3][begin_i].global_position, delta)
+		begin_i += 1
 
 func _rotate_point_cloud(delta : float, mag : float) -> void:
-	pivot.rotation+=mag*delta
+	pivot.rotation += mag * delta
 
 func _calculate_box_logic(box, target_point : Vector2, delta) -> void:
-	box.position=lerp(box.position, target_point, 4.4*delta)
+	box.position = lerp(box.position, target_point, 4.4 * delta)
 
-func _update_box_list() -> void:
-	pass #add boxes to box list when needed...
+func _update_box_list(box, id) -> void:
+	if box.get_meta("id") not in box_list.keys():
+		box_list[id] = box
+	else:
+		print("That box is already in the list!")
--- scripts/gdscript/box.gd
+++ scripts/gdscript/box.gd
@@ -1,1 +1,28 @@
 extends Node2D
+
+@onready var glowish: Sprite2D = $Area2D/glowish
+var canClick = false
+
+signal clicked(Node2D, int)
+
+func _ready():
+	glowish.visible = false
+	
+func _process(delta: float) -> void:
+	if canClick and Input.is_action_just_pressed("leftClick"):
+		if self.get_meta("id") == null:
+			var id = randi()
+			self.set_meta("id", id)
+			clicked.emit(self, id)
+		else:
+			clicked.emit(self, self.get_meta("id"))
+	
+
+func _on_area_2d_mouse_entered() -> void:
+	glowish.visible = true
+	canClick = true
+	
+
+func _on_area_2d_mouse_exited() -> void:
+	glowish.visible = false
+	canClick = false
