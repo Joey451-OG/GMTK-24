ee01bb23db2f484c25ace70079164f1d5b3356bd
ItIsJoeyG <itsjoeygofficial@outlook.com>
2024-08-17 23:30:51 -0500

    Allowed the player to scale with the scally scale scale gun

--- scripts/gdscript/gun_script.gd
+++ scripts/gdscript/gun_script.gd
@@ -1,22 +1,46 @@
 extends Node2D
 
+@onready var marker_2d: Marker2D = $Pivot_Point/Box_SnapPos
+@onready var target_box : Node2D
+
+var isHoldingBox := false
+var isInPosition := false
 var t_angle: float
 
+signal send_package(Node2D)
+
 func _process(delta):
-	if Input.is_action_just_pressed("leftClick"):
+	if Input.is_action_just_pressed("leftClick") and isHoldingBox:
 		_fire_animation()
 	_rotate_gun()
 	_update_position(delta)
-	t_angle = $Pivot.rotation
+	t_angle = $Pivot_Point.rotation
+	
+	# Snapping box to marker pos
+	if target_box != null:
+		if !isInPosition:
+			target_box.global_position = lerp(target_box.global_position, marker_2d.global_position, 4.4 * delta)
+		else:
+			target_box.global_position = marker_2d.global_position
+		
+		if (target_box.global_position.x - marker_2d.global_position.x) < 0.01:
+			isInPosition = true
 
 func _rotate_gun() -> void:
-	$Pivot.look_at(get_global_mouse_position())
+	$Pivot_Point.look_at(get_global_mouse_position())
 func _update_position(delta : float) -> void:
-	$Pivot/Mesh.position = lerp($Pivot/Mesh.position, Vector2(13.0,0.0), 4.0 * delta);
+	$Pivot_Point/Mesh.position = lerp($Pivot_Point/Mesh.position, Vector2(13.0,0.0), 4.0 * delta);
 
 func _get_firing_angle() -> float:
 	return t_angle
 func _fire_animation() -> void:
-	$Pivot/Mesh.position -= Vector2(15.0,0.0)
+	$Pivot_Point/Mesh.position -= Vector2(15.0,0.0)
 func _get_fire_position()-> Marker2D:
-	return $Pivot/Bullet_Point
+	return $Pivot_Point/Bullet_Point
+
+# signal functions
+func _capture_box(box, id):
+	if target_box == null:
+		target_box = box
+		isHoldingBox = true
+		send_package.emit(box)
--- scripts/gdscript/level.gd
+++ scripts/gdscript/level.gd
@@ -1,18 +1,62 @@
 extends Node2D
 
+@export var scale_factor := 0.2
+@export var thrown_speed := 2.0
+@export var bullet_time_amount := 20
+@export var time_scale := 0.2
+@export var bullet_time_drain_rate := 0.2
 
+@onready var bt_amount = bullet_time_amount
+@onready var bt_drain_rate = 1 - bullet_time_drain_rate
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var tracked_box : Node2D
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if tracked_box != null : _scale_box(delta)
 
 
-func _enimy_request_id(request_id) -> void:
-	var target = instance_from_id(request_id)
+func _scale_box(detla: float):
+	# turn the glow effect on to show which box is selected
+	tracked_box.glowish.visible = true
 	
-	target._recive_answer($PlayerBody/AOE.list_old_ids[-1])
+	# Scale up or down based on scroll whell
+	if Input.is_action_just_pressed("scrollUp"):
+		tracked_box.scale *= scale_factor + 1 # lerp this eventually
+		thrown_speed *= 1 - scale_factor
+	elif Input.is_action_just_pressed("scrollDown"):
+		tracked_box.scale *= 1 - scale_factor # lerp eventually
+		thrown_speed *= 1 + scale_factor
+	
+	# Bullet Time
+	elif Input.is_action_pressed("rightClick"):
+		#enter_bullet_time.emit()
+		if bt_amount > 0.00000001:
+			
+			# slow down time
+			Engine.time_scale = time_scale
+			bt_amount *= bt_drain_rate
+			tracked_box.set_meta("notInBT", false)
+		else:
+			# same as below elif without recharging the bullet time
+			Engine.time_scale = 1
+			# add code once done below
+			tracked_box.set_meta("isThrown", true)
+			tracked_box.set_meta("notInBT", false)
+			if !tracked_box.get_meta("isFired"):
+				tracked_box.set_meta("isFired", true)
+			
+	elif Input.is_action_just_released("rightClick"):
+		# reset bulllet time and fling das Block!
+		#exit_bullet_time.emit()
+		Engine.time_scale = 1
+		bt_amount = bullet_time_amount
+		
+		# add a physics object to the box and apply velocity_vector
+		tracked_box.set_meta("isThrown", true)
+		tracked_box.set_meta("notInBT", false)
+		if !tracked_box.get_meta("isFired"):
+				tracked_box.set_meta("isFired", true)
+
+
+func _recive_box(box):
+	tracked_box = box
