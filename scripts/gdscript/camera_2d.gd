094cdab19406a4d082bdc0b7d49f1f9405b7e4c4
ItIsJoeyG <itsjoeygofficial@outlook.com>
2024-08-19 13:28:29 -0500

    Fix error in l0 and added l1

--- scripts/gdscript/camera_2d.gd
+++ scripts/gdscript/camera_2d.gd
@@ -3,14 +3,27 @@ extends Node2D
 @export var cameras : Array[Camera2D]
 @export var player : CharacterBody2D
 
+var valid_ids = []
 var section_index := 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	cameras[0].enabled = true
+	valid_ids.append(player.get_instance_id())
 
-
+func _on_next_0_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") or body.get_instance_id() in valid_ids:
+		valid_ids.append(body.get_instance_id())
+		cameras[0].enabled = true
+		cameras[1].enabled = false
+	
+	for cam in cameras:
+		print(cam.enabled)
 
 func _on_next_1(body: Node2D) -> void:
-	if body.is_in_group("player"):	
+	if body.is_in_group("player") or body.get_instance_id() in valid_ids:
+		valid_ids.append(body.get_instance_id())
 		cameras[0].enabled = false
-		cameras[0].enabled = true
+		cameras[1].enabled = true
+	
+	for cam in cameras:
+		print(cam.enabled)
